9e52ac3b15ac0f4f807a813785393071dd3f2c72
Furqan <macbookair@Furqans-MacBook-2021.local>
2024-06-08 01:11:38 +0500

    changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:8800/api/',
-  baseURL: 'https://rgcm2-11d897d3388a.herokuapp.com/api/',
+  baseURL: process.env.REACT_APP_API_URL,
 })
 // })
--- server/index.js
+++ server/index.js
@@ -10,18 +10,7 @@ import authRoute from './routes/auth.js'
 import bookingRoute from './routes/booking.js'
 const __dirname = path.resolve()
 const app = express()
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  )
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-  res.setHeader('Access-Control-Allow-Credentials', true)
-
-  next()
-})
+app.use(cors())
 dotenv.config()
 app.use(cookieParser())
 app.use(express.json())
